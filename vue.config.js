1ee1ad5d0a9c0aa9f0a26b5b223aad2c5a624e67
Muhun Kim <iam@muhun.kim>
2019-07-14 14:44:32 +0900

    remove duplicate line: themeColor, background_color
    this prop follow in the manifest file.

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,6 @@ module.exports = {
         workboxPluginMode: 'InjectManifest',
         workboxOptions: {
             swSrc: 'public/service-worker.js'
-        },
-		themeColor: '#F1F1F1',
-		background_color: '#F1F1F1'
+        }
     }
 }
