d66bc5f03bc7252d581031db221235784aa47869
Muhun Kim <iam@muhun.kim>
2019-07-24 03:27:16 +0900

    change service worker script file name to 'sw' and use dev mode in deploy
    원인을 모르겠지만 프로덕션 모드에선 프리캐싱이 안된다.

--- src/api.js
+++ src/api.js
@@ -2,9 +2,10 @@ import { get } from 'axios'
 
 export default function() {
     return get(
-        process.env.NODE_ENV === 'development'
-            ? 'https://meals-data.muhun.kim/dev'
-            : 'https://meals-data.muhun.kim'
+		'https://meals-data.muhun.kim'
+        // process.env.NODE_ENV === 'development'
+        //     ? 'https://meals-data.muhun.kim/dev'
+        //     : 'https://meals-data.muhun.kim/dev'
     )
         .then(res => res.data)
         .catch(res => {
