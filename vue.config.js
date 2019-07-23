3fafd1d627b2f007a4e0994371c5a4e1dc75ebb7
Muhun Kim <iam@muhun.kim>
2019-07-24 01:11:58 +0900

    remove vue-pwa depedencies

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,5 @@
 // vue.config.js
 module.exports = {
-    pwa: {
-        themeColor: '#F1F1F1',
-        background_color: '#F1F1F1'
-	},
 	filenameHashing: false,
     chainWebpack: config => {
         const svgRule = config.module.rule('svg')
