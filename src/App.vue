26e7f770d74e29597bd339246e373b779262002a
Muhun Kim <iam@muhun.kim>
2019-08-21 16:21:37 +0900

    데이터 갱신 대기 혹은 요청하는 로직 구현함

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,14 @@
         <md-toolbar md-elevation="3">
             <h3 class="md-title">{{ week }}</h3>
         </md-toolbar>
-        <panel v-if="isWeekday" :list="today_meal" :open.sync="time" />
+        <template v-if="isWeekday">
+            <panel v-if="isCacheVaild" :list="today_meal" :open.sync="time" />
+            <div v-else-if="isBeforeTenOclock">10시 이후에 새로운 학식 데이터가 업로드 됩니다.</div>
+            <div v-else="!isCacheVaild">네트워크 연결이 필요합니다.</div>
+        </template>
         <div v-else class="weekend">
             <NpVacation />주말
         </div>
-        <!-- <dialog :flag="false" /> -->
         <bottom-nav :time.sync="time" />
     </div>
 </template>
@@ -19,6 +22,16 @@ import Panel from './components/Panel.vue'
 import BottomNav from './components/BottomNav.vue'
 import NpVacation from './SVG/npVacation.svg'
 
+const weekDict = {
+    0: '일요일',
+    1: '월요일',
+    2: '화요일',
+    3: '수요일',
+    4: '목요일',
+    5: '금요일',
+    6: '토요일'
+}
+
 export default {
     name: 'App',
     components: {
@@ -28,32 +41,41 @@ export default {
         NpVacation
     },
     data: () => ({
+        cache_keys: [],
+        isBeforeTenOclock: false,
         isWeekday: true,
-        weekDict: {
-            0: '일요일',
-            1: '월요일',
-            2: '화요일',
-            3: '수요일',
-            4: '목요일',
-            5: '금요일',
-            6: '토요일'
-        },
         week: '월요일',
         time: '점심',
         timeKey: { 점심: [true, false], 저녁: [false, true] },
         panel: [true, false],
+        showDialog: false,
         // weekly_meal: {}, //TODO
         today_meal: {}
     }),
+    computed: {
+        isCacheVaild() {
+            const api_v = `api_${Math.floor(new Date().getDate() / 7)}`
+            return this.cache_keys.findIndex(itm => itm == api_v) > -1
+        }
+    },
     created() {
+        caches.keys().then(keys => (this.cache_keys = keys))
+
         const date = new Date()
         const weekday = date.getDay()
-        this.week = this.weekDict[weekday]
+        const Hour = date.getHours()
+
+        if (weekday === 1) this.isBeforeTenOclock = 10 > Hour
+
+        this.week = weekDict[weekday]
+
         if (weekday === 0 || weekday === 6) {
             this.isWeekday = false
             return
         }
-        this.time = date.getHours() < 15 ? '점심' : '저녁'
+
+        this.time = Hour < 15 ? '점심' : '저녁'
+
         request().then(data => {
             // this.weekly_meals = data
             this.today_meal = data[weekday - 1]
@@ -69,7 +91,7 @@ export default {
 
 <style scoped>
 .md-toolbar {
-	justify-content: center;
+    justify-content: center;
 }
 
 .weekend {
