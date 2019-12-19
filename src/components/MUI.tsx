22421cb70fe73fdb444ccfd036fba92095c32a24
Muhun Kim <iam@muhun.kim>
2019-12-19 13:42:59 +0900

    Splite Components

---
+++ src/components/MUI.tsx
@@ -0,0 +1,85 @@
+import React from 'react'
+
+import { styled } from '@material-ui/core/styles'
+
+import {
+  List,
+  ListItem,
+  ListItemText,
+  ExpansionPanelSummary,
+  Typography,
+  Paper,
+  Fab,
+  BottomNavigation,
+  BottomNavigationAction as Action
+} from '@material-ui/core'
+
+import {
+  Launch as LunchIcon,
+  Restaurant as DinnerIcon,
+  Info as InfoIcon,
+  ExpandMore
+} from '@material-ui/icons'
+
+import { WeekKey } from '../@types'
+
+export const Item = styled(ListItem)({
+  padding: '4px 24px'
+})
+
+export const ItemText = ({ menu, type }: { menu: string[]; type: string }) => (
+  <ListItemText primary={menu.join(', ')} secondary={type} />
+)
+
+export const PanelByTime = ({ time }: { time: string }) => (
+  <ExpansionPanelSummary
+    expandIcon={<ExpandMore />}
+    aria-controls="panel1a-content"
+    id="panel1a-header"
+  >
+    <Typography>{time}</Typography>
+  </ExpansionPanelSummary>
+)
+
+export const Ul = styled(List)({
+  marginBottom: '2rem'
+})
+
+export const TodayOfWeek = ({ weekday }: { weekday: WeekKey }) => (
+  <Typography variant="h5" style={{ padding: '2rem 2rem 2rem' }} gutterBottom>
+    {weekday}
+  </Typography>
+)
+
+export const Wrapper = styled(Paper)({ height: '100%' })
+
+export const BottomNav = styled(BottomNavigation)({
+  top: 'auto',
+  bottom: 0,
+  justifyContent: 'space-around',
+  position: 'fixed',
+  width: '100%'
+})
+
+export const Info = () => (
+  <Fab
+    color="secondary"
+    aria-label="add"
+    style={{
+      position: 'absolute',
+      zIndex: 1,
+      bottom: 28,
+      left: 0,
+      right: 0,
+      margin: '0 auto'
+    }}
+  >
+    <InfoIcon />
+  </Fab>
+)
+
+export const Lunch = () => <Action label="점심" icon={<LunchIcon />} />
+
+export const Dinner = () => (
+  <Action label="저녁" icon={<DinnerIcon color={'secondary'} />} />
+)
