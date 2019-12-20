51d1c9bb2c721a7a7369a373e7bc2db3d31d29f3
Muhun Kim <iam@muhun.kim>
2019-12-20 23:58:08 +0900

    Add service-worker

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import './styles/theme.scss'
 import { Provider } from 'react-redux'
 import store from './store'
 
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <Provider store={store}>
@@ -19,4 +19,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister()
+serviceWorker.register()
