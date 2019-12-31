908bafefe1d79a8e48ce00880465984170ac6a43
Muhun Kim <iam@muhun.kim>
2019-12-31 06:53:31 -0800

    Use webpack

---
+++ webpack.config.js
@@ -0,0 +1,138 @@
+const webpack = require('webpack')
+const path = require('path')
+const package = require('./package.json')
+
+const isProduction =
+  process.argv.indexOf('-p') >= 0 || process.env.NODE_ENV === 'production'
+const sourcePath = path.join(__dirname, './src')
+const outPath = path.join(__dirname, './dist')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  context: sourcePath,
+  entry: {
+    app: './index.tsx'
+  },
+  output: {
+    path: outPath,
+    filename: 'main.js',
+    chunkFilename: '[name].js'
+  },
+  target: 'web',
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+    mainFields: ['module', 'browser', 'main'],
+    alias: {
+      app: path.resolve(__dirname, 'src/app/')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          !isProduction && {
+            loader: 'babel-loader',
+            options: { plugins: ['react-hot-loader/babel'] }
+          },
+          'ts-loader'
+        ].filter(Boolean)
+      },
+      // css
+      {
+        test: /\.css$/,
+        use: [
+          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+          {
+            loader: 'css-loader',
+            query: {
+              modules: true,
+              sourceMap: !isProduction,
+              importLoaders: 1,
+              localIdentName: isProduction
+                ? '[hash:base64:5]'
+                : '[local]__[hash:base64:5]'
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [
+                require('postcss-import')({ addDependencyTo: webpack }),
+                require('postcss-url')(),
+                require('postcss-preset-env')({
+                  stage: 2
+                }),
+                require('postcss-reporter')(),
+                require('postcss-browser-reporter')({
+                  disabled: isProduction
+                })
+              ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpe?g|gif|bmp|mp3|mp4|ogg|wav|eot|ttf|woff|woff2)$/,
+        use: 'file-loader'
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      name: true,
+      cacheGroups: {
+        commons: {
+          chunks: 'initial',
+          minChunks: 2
+        },
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          chunks: 'all',
+          filename: 'vendor.js',
+          priority: -10
+        }
+      }
+    },
+    runtimeChunk: true
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'development', // use 'development' unless process.env.NODE_ENV is defined
+      DEBUG: false
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'app.css',
+      disable: !isProduction
+    }),
+    new CopyWebpackPlugin([
+      // static files to the site root folder (index and robots)
+      {
+        from: path.join(__dirname, './public'),
+        to: outPath,
+        toType: 'dir'
+      }
+    ])
+  ],
+  devServer: {
+    contentBase: sourcePath,
+    hot: true,
+    inline: true,
+    historyApiFallback: {
+      disableDotRule: true
+    },
+    stats: 'minimal',
+    clientLogLevel: 'warning'
+  },
+  devtool: isProduction ? 'hidden-source-map' : 'cheap-module-eval-source-map',
+  node: {
+    fs: 'empty',
+    net: 'empty'
+  }
+}
