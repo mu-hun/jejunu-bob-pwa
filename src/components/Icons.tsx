b24755448407e5b1a75efab198a545c1c9c9d352
Muhun Kim <iam@muhun.kim>
2020-05-27 15:46:55 +0900

    Remove semicolon in Icons component

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
 import {
   WeekendOutlined as WeekendIcon,
@@ -15,36 +15,36 @@ export const Div = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`;
+`
 
 export const Label = styled.label`
   font-size: 2em;
-`;
+`
 
 export const Weekend = () => (
   <Div>
     <WeekendIcon style={{ fontSize: '15em' }} />
     <Label>주말</Label>
   </Div>
-);
+)
 
 export const Waiting = () => (
   <Div>
     <AccessTimeIcon style={{ fontSize: '15em' }} />
     <Label>아직 새로운 식단을 보여줄 준비가 안되어 있어요!</Label>
   </Div>
-);
+)
 
 export const NoInternet = () => (
   <Div>
     <NoInternetIcon style={{ fontSize: '15em' }} />
     <Label>인터넷 연결이 필요합니다.</Label>
   </Div>
-);
+)
 
 export const RequestError = () => (
   <Div>
     <ErrorIcon style={{ fontSize: '15em' }} />
     <Label>학식 정보를 가져오는 데에 실패했어요.</Label>
   </Div>
-);
+)
